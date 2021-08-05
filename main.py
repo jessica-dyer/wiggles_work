0e296796bd459c57d171fcaff5727406dc1a6296
jessica-dyer <jessica.dyer@gmail.com>
2021-08-04 19:49:29 -0700

    Reading and writing JSON to file working.

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-class Route:
-    def __init__(self, name, grade):
+import json
+from abc import ABC, abstractmethod
+class JsonMappable(ABC):
+    @abstractmethod
+    def to_dict(self):
+        pass
+
+class Route(JsonMappable):
+    def __init__(self, name, grade, crag):
         self.name = name
         self.grade = grade
+        self.crag = crag
+
+    def to_dict(self):
+        dictionary = {}
+        dictionary["name"] = self.name
+        dictionary["grade"] = self.grade
+        dictionary["crag"] = self.crag
+        return dictionary
+
+    @classmethod
+    def construct_from_dict(cls, dictionary):
+        return Route(dictionary["name"],
+                     dictionary["grade"],
+                     dictionary["crag"])
 
     def pretty_print(self):
         """Nicely prints the route name and grade."""
-        print(f"The route name is {self.name} and the grade is {self.grade}")
+        print(f"The route name is {self.name} and the grade is {self.grade}. This route is located at the {self.crag} crag.")
 
 
-class RouteList:
+class RouteList(JsonMappable):
     def __init__(self):
         self.routes = []
+
+    def to_dict(self):
+        route_dictionaries = []
+        for r in self.routes:
+            route_dictionaries.append(r.to_dict())
+        dictionary = {}
+        dictionary["routes"] = route_dictionaries
+        return dictionary
 
     def add_route(self, new_route):
         self.routes.append(new_route)
@@ -19,17 +48,26 @@ class RouteList:
         for r in self.routes:
             r.pretty_print()
 
-    def export_to_file(self, filename):
-        # This will export all routes to JSON file
+    def export_to_file(self, filename = 'saved_routes.json'):
+        dictionary = self.to_dict()
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(dictionary, f, ensure_ascii=False, indent=4)
 
-    def import_from_file(self, filename):
-        # to do--We want the routes to import from JSON file
+    def import_from_file(self, filename = 'saved_routes.json'):
+        with open(filename, 'r', encoding='utf-8') as f:
+            dictionary = json.load(f)
+            route_dictionaries = dictionary["routes"]
+            for d in route_dictionaries:
+                route = Route.construct_from_dict(d)
+                self.routes.append(route)
 
 def test():
-    x = Route("Outer Space", "5.9+")
+    # x = Route("Outer Space", "5.9+", "Snow Creek")
     master_list = RouteList()
-    master_list.add_route(x)
+    # master_list.add_route(x)
+    master_list.import_from_file()
     master_list.pretty_print_all()
+    master_list.export_to_file()
 
 
 test()
