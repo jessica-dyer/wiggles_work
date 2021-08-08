86cc64e3a3532c8bcfe921a585d6bf1341f806a4
jessica-dyer <jessica.dyer@gmail.com>
2021-08-08 10:03:21 -0700

    We moved class member variables back to init and hooked up "no" button on delete confirmation screen.

--- wiggles_work_app/ui/delete_confirmation_screen.py
+++ wiggles_work_app/ui/delete_confirmation_screen.py
@@ -23,25 +23,23 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 
 class DeleteConfirmationScreen(GridLayout):
-    name = None #A string; the name of the thing to be deleted
-    back_screen = None #A widget; the screen we want to return to if delete is cancelled
-    delete_action = None #A function; the function that will be called when delete is confirmed
-    label = Label(text="")
-    button_container = GridLayout(cols=2)
-    no_button = Button(text="No")
-    yes_button = Button(text="Yes")
-
+    
     def __init__(self, wiggles_work_app, name, back_screen, delete_action):
         super().__init__(rows=2)
         self.wiggles_work_app = wiggles_work_app
-        self.name = name
-        self.back_screen = back_screen
-        self.delete_action = delete_action
+        self.name = name #A string; the name of the thing to be deleted
+        self.back_screen = back_screen #A widget; the screen we want to return to if delete is cancelled
+        self.delete_action = delete_action #A function; the function that will be called when delete is confirmed
+        self.label = Label(text="")
+        self.button_container = GridLayout(cols=2)
+        self.no_button = Button(text="No")
+        self.yes_button = Button(text="Yes")
         self.add_widget(self.label)
         self.label.text = "Are you sure you want to delete " + name + "?"
         self.add_widget(self.button_container)
         self.button_container.add_widget(self.no_button)
         self.button_container.add_widget(self.yes_button)
+        self.no_button.bind(on_press=self.on_click_no)
 
     def on_click_no(self, button):
-        pass
+        self.wiggles_work_app.window.set_view(self.back_screen)
