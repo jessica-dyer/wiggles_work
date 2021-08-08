fddbaacd7fe0e9bf7459797f120ef5034c861297
jessica-dyer <jessica.dyer@gmail.com>
2021-08-07 20:19:33 -0700

    Finally!  Got row highlight to clear after touch-up.

--- wiggles_work_app/ui/route_list_row_view.py
+++ wiggles_work_app/ui/route_list_row_view.py
@@ -1,8 +1,12 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.properties import BooleanProperty
 from kivy.graphics import Color, Rectangle
+from kivy.uix.widget import Widget
+
 
 class RouteListRowView(RecycleDataViewBehavior, GridLayout):
     wiggles_work_app = None
@@ -11,32 +15,35 @@ class RouteListRowView(RecycleDataViewBehavior, GridLayout):
     selectable = BooleanProperty(True)
 
     def __init__(self):
-        super().__init__(cols=5)
+        super().__init__(cols=1)
+        self.grid_layout = GridLayout(cols=5)
+        self.add_widget(self.grid_layout, index=0)
+        self.backgroundView = Widget()
+        self.add_widget(self.backgroundView, 1)
         self.route = None
-        #self.label = Label(text=route.name)
-        self.label = Label(text="Foo")
-        self.add_widget(self.label)
+        self.label = Label(text="")
+        self.grid_layout.add_widget(self.label)
+
 
     def refresh_view_attrs(self, rv, index, data_item):
         self.wiggles_work_app = rv.wiggles_work_app
         self.index = index
         self.route = data_item["route"]
         self.label.text = self.route.name
+        self.size = self.grid_layout.size
 
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos) and self.selectable:
-            with self.canvas:
+            with self.backgroundView.canvas:
                 Color(1,1,1,0.75)   # color for highlight on click
                 Rectangle(pos=self.pos, size=self.size)
 
     def on_touch_up(self, touch):
         if self.collide_point(*touch.pos) and self.selectable:
             self.onRowClicked()
-            # with self.canvas:
-            #     Color(0,0,0,1)      # color to revert too after highlight goes away
-            #     Rectangle(pos=self.pos, size=self.size)
+            self.backgroundView.canvas.clear()
 
-        return super(GridLayout, self).on_touch_up(touch)
 
     def onRowClicked(self):
+        print("Navigating to route " + self.route.name)
         self.wiggles_work_app.navigate_to_edit_route(self.route)
