0e0c1f720df2c3c69c0f91e581bd7d23270684e1
jessica-dyer <jessica.dyer@gmail.com>
2021-08-25 21:21:12 -0700

    Position route grade & route name labels within a row so they look like two different columns.

--- wiggles_work_app/ui/route_list_row_view.py
+++ wiggles_work_app/ui/route_list_row_view.py
@@ -1,4 +1,5 @@
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.properties import BooleanProperty
 from kivy.graphics import Color, Rectangle
@@ -6,6 +7,7 @@ from kivy.uix.widget import Widget
 from ui.color import WWColors
 from kivymd.uix.label import MDLabel
 from kivymd.uix.list import *
+from background_color_debugging import *
 
 
 class RouteListRowView(RecycleDataViewBehavior, TwoLineListItem):
@@ -16,13 +18,15 @@ class RouteListRowView(RecycleDataViewBehavior, TwoLineListItem):
 
     def __init__(self):
         super().__init__()
-        self.row_layout = GridLayout(rows=1)
+        self.row_layout = BoxLayout(orientation="horizontal")
         self.add_widget(self.row_layout, index=0)
         self.backgroundView = Widget()
         self.add_widget(self.backgroundView, 1)
         self.route = None
         self.name_label = MDLabel(text="")
         self.grade_label = MDLabel(text="", halign='center')
+        self.grade_label.size_hint_x = None
+        self.grade_label.width = 80
         self.row_layout.add_widget(self.grade_label)
         self.row_layout.add_widget(self.name_label)
 
@@ -30,11 +34,13 @@ class RouteListRowView(RecycleDataViewBehavior, TwoLineListItem):
         self.wiggles_work_app = rv.wiggles_work_app
         self.index = index
         self.route = data_item["route"]
-        # self.name_label.text = self.route.name
-        self.text = self.route.name
-        # self.grade_label.text = self.route.grade
-        self.secondary_text = self.route.grade
-        self.size = self.row_layout.size
+        self.name_label.text = self.route.name
+        self.grade_label.text = self.route.grade
+
+    def on_pos(self, widget, pos):
+        self.row_layout.size_hint = (1, None)
+        self.row_layout.height = self.height
+        self.row_layout.pos = self.pos
 
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos) and self.selectable:
