e047fa805e5ec9c45215e55dce935df07bab006f
jessica-dyer <jessica.dyer@gmail.com>
2021-10-24 17:28:31 -0700

    Progress on edit ascent screen.

--- wiggles_work_app/ui/edit_ascent_screen.py
+++ wiggles_work_app/ui/edit_ascent_screen.py
@@ -7,6 +7,9 @@ from data_structures.ascent import *
 from ui.view_route_screen import *
 from kivymd.uix.label import MDLabel
 from kivymd.uix.menu import MDDropdownMenu
+from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.textfield import MDTextField
+
 from kivymd.uix.dropdownitem import MDDropDownItem
 
 
@@ -31,31 +34,56 @@ class EditAscentScreen(WigglesWorkScreen):
             self.mode = AscentScreenMode.EDIT
 
         self.wiggles_work_app = wiggles_work_app
-        self.screen_header = MDLabel(text="ADD & EDIT ASCENT",
-                                     halign='center',
-                                     font_style='H4')
+        self.route_card = MDCard(orientation='vertical',
+                                 padding=8,
+                                 size_hint=(1, None),
+                                 height=300,
+                                 pos_hint={"center_y": .5},
+                                 focus_behavior=True)
+        current_route = self.ascent.get_route()
+        current_route_name = current_route.name
+
+        self.route_name = MDLabel(text=f'Route name: {current_route_name}')
+        self.ascent_type_label = MDLabel(text='')
+        self.update_ascent_type_label()
+        self.date_label = MDLabel(text=f'Date climbed: {self.ascent.date}')
         self.save_ascent_button = MDFillRoundFlatButton(text="Save")
-        self.screen_content.add_widget(self.screen_header)
         self.screen_content.add_widget(self.save_ascent_button)
         self.save_ascent_button.bind(on_press=self.on_click_save_ascent)
         self.toolbar.left_action_items = [["arrow-left-bold", lambda x: self.on_click_back()]]
+        self.notes_field = MDTextField(hint_text='Notes from the ascent:',
+                                       mode='fill',
+                                       fill_color=(0, 0, 0, .2),
+                                       multiline=True)
+        self.ascent_type_container = BoxLayout(orientation='horizontal')
+        self.ascent_date_container = BoxLayout(orientation='horizontal')
 
+        self.route_card.add_widget(self.route_name)
+
+        self.ascent_type_container.add_widget(self.ascent_type_label)
+        self.ascent_date_container.add_widget(self.date_label)
+
+        self.route_card.add_widget(self.ascent_type_container)
+        self.route_card.add_widget(self.ascent_date_container)
+
+        self.screen_content.add_widget(self.route_card)
+        self.screen_content.add_widget(self.notes_field)
         self.select_date = MDFillRoundFlatButton(text="Select date")
         self.select_date.bind(on_press=self.show_date_picker_dialog)
-        self.add_widget(self.select_date)
 
-        self.ascent_type_button = MDFillRoundFlatButton(text="Foo")
+        self.ascent_type_button = MDFillRoundFlatButton(text="Change")
         self.ascent_type_button.bind(on_press=self.on_dropdown_opened)
-        self.screen_content.add_widget(self.ascent_type_button)
 
         self.ascent_type_menu = MDDropdownMenu(caller=self.ascent_type_button,
                                                items=self.build_ascent_type_menu_items(),
                                                position="center",
                                                width_mult=4)
+        self.ascent_type_container.add_widget(self.ascent_type_button)
+        self.ascent_date_container.add_widget(self.select_date)
 
     def build_ascent_type_menu_items(self):
         # all_values = AscentType.__members__.values()
-        foo =[]
+        foo = []
         for type in AscentType:
             dict = {
                 "text": type.name_for_dropdown(),
@@ -67,10 +95,21 @@ class EditAscentScreen(WigglesWorkScreen):
     def on_dropdown_item_selected(self, selected_type):
         # self.ascent_type_button.(self.ascent_type_menu.text)
         self.ascent.ascent_type = selected_type
+        self.update_ascent_type_label()
         self.ascent_type_menu.dismiss()
 
     def on_dropdown_opened(self, arg2):
         self.ascent_type_menu.open()
+
+    def update_ascent_type_label(self):
+        userFacingAscentType = "Not yet set"
+        if self.ascent.ascent_type is None:
+            pass
+        else:
+            userFacingAscentType = self.ascent.ascent_type.name_for_dropdown()
+
+        self.ascent_type_label.text = f'Ascent type: {userFacingAscentType}'
+
 
     def on_date_picked(self, datePicker, dateObject, someOtherCrap):
         self.ascent.date = dateObject
