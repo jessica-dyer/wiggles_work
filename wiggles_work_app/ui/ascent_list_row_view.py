957ecb209fe75c70162df0f533297789d8a81f33
jessica-dyer <jessica.dyer@gmail.com>
2021-10-24 19:45:27 -0700

    Date of each ascent now showing on ascent list view.

--- wiggles_work_app/ui/ascent_list_row_view.py
+++ wiggles_work_app/ui/ascent_list_row_view.py
@@ -1,4 +1,5 @@
 import kivy
+
 kivy.require("1.9.1")
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
@@ -9,6 +10,7 @@ from kivy.base import Builder
 from ui.edit_route_screen import *
 from wiggles_work_app import *
 from ui.wiggles_row_view import WigglesRowView
+from data_structures.ascent import *
 from kivymd.uix.label import MDLabel
 
 
@@ -27,7 +29,7 @@ class AscentListRowView(WigglesRowView):
         self.wiggles_work_app = rv.wiggles_work_app
         self.index = index
         self.ascent = data_item["ascent"]
-        self.name_label.text = "Put something here"
+        self.name_label.text = self.ascent.date.strftime(Ascent.user_facing_day_format)
         # self.name_label.text = self.ascent.id
         # self.grade_label.text = self.route.grade
         self.size = self.row_layout.size
